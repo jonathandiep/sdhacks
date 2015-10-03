5a989ea2a65ac24968a1321005f7e2dc2a6e509d
Darren Eng <darrenle9@gmail.com>
2015-10-03 11:16:12 -0700

    Add Schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var Twit = require('twit');
+var User = require('./user'); // new
 
 var T = new Twit({
   consumer_key: 'lGzemE6STP9kvBaHVhiWJuDD0',

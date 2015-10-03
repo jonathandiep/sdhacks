6efb9027f7a08c6d16624d629fc735ace022340a
Jonathan Diep <jonathandiep@hotmail.com>
2015-10-03 01:45:08 -0700

    added auto response

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var Twit = require('twit');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:/')
 
 var T = new Twit({
   consumer_key: 'lGzemE6STP9kvBaHVhiWJuDD0',
@@ -9,6 +12,20 @@ var T = new Twit({
   access_token_secret: 'o2yqzmNhe4XTkF2FApB3e2Sot8HfztjoKQvLMgaAizZx8'
 })
 
+/*
 T.post('statuses/update', { status: 'testing!' }, function(err, data, response) {
   console.log(data);
 })
+*/
+
+console.log('working');
+
+var stream = T.stream('statuses/filter', {track: '#sdhackstweettesting'});
+
+stream.on('tweet', function(tweet) {
+  var username = tweet.user.screen_name
+  console.log(username);
+  T.post('statuses/update', { status: '@' + username + ' this is a response!' }, function(err, data, response) {
+    if (err) return err;
+  });
+});
